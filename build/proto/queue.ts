72c7a8d5804fa03ba0cfb7ccf8f5037cc6b94537
G10N(nick) <nick.supakorn@hotmail.com>
2023-11-08 00:07:39 +0700

    update queue proto

--- build/proto/queue.ts
+++ build/proto/queue.ts
@@ -18,19 +18,36 @@ export interface Order {
   menus: Menu[];
 }
 
+export interface RestaurantId {
+  id: number;
+}
+
+export interface AcceptOrderDto {
+  accept: boolean;
+  resId: number;
+}
+
 export const QUEUE_PACKAGE_NAME = "queue";
 
 export interface QueueServiceClient {
   createOrder(request: Order): Observable<Order>;
+
+  consumeQueue(request: RestaurantId): Observable<Order>;
+
+  acceptOrder(request: AcceptOrderDto): Observable<Order>;
 }
 
 export interface QueueServiceController {
   createOrder(request: Order): Promise<Order> | Observable<Order> | Order;
+
+  consumeQueue(request: RestaurantId): Promise<Order> | Observable<Order> | Order;
+
+  acceptOrder(request: AcceptOrderDto): Promise<Order> | Observable<Order> | Order;
 }
 
 export function QueueServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = ["createOrder"];
+    const grpcMethods: string[] = ["createOrder", "consumeQueue", "acceptOrder"];
     for (const method of grpcMethods) {
       const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
       GrpcMethod("QueueService", method)(constructor.prototype[method], method, descriptor);
